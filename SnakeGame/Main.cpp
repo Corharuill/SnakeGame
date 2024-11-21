6b38cb15225feac9c64d133255de764a27f35df3
Stanislav Vereshagin <Corharuill@yandex.ru>
2024-11-22 00:04:44 +0300

    Все 4 стенки

--- SnakeGame/Main.cpp
+++ SnakeGame/Main.cpp
@@ -1,68 +1,74 @@
 ﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
 
 const int widht = 20;
 const int height = 20;
+int x, y, fruitX, fruitY;
+int i, j;
 
-void Draw() 
+void Setup()
 {
-	
+	fruitX = rand() % widht;
+	fruitY = rand() % height;
+}
+void Draw()
+{
 
-	std::cout << std::endl;
-	for (int y = 0; y < height; y++) 
+	for (int y = 0; y <= height; y++)
 	{
-		
-		std::cout << "+|"; //если не заполнет пустотой то рисует стенку (левую) и дльше в цикле отрисовки стенки по кординатам х и у он заполняет пустотой и дорисовывает правую стенку если удволетворены условия if
-		
-		
-		
-		for (int x = 0; x < widht; x++)
+		for (int x = 0; x <= widht; x++)
 		{
-			
-			
-			if (x == 0, x < height - 1) //заполняет пустотой расстояние от стенки до стенки
-			{
 
+			if (y == 0)
+				std::cout << "1";
+			if (i == y && j == x)
+			{
+				std::cout << "@";
+			}
+			else if (y == height)
+			{
+				std::cout << "2";
+			}
+			else if (x == 0 && y != widht)
+			{
+				std::cout << "3";
+			}
+			else if (x == height && y != widht)
+			{
+				std::cout << "4";
+			}
+			else if (y != 0 && y != widht)
+			{
 				std::cout << " ";
 			}
-			else 
-				std::cout << "|+"; //если не заполнет пустотой то рисует стенку (правую)
-
-			
-			
+			else if (x == fruitX && y == fruitY)
+			{
+				std::cout << "F";
+			}
 		}
 		std::cout << std::endl;
 	}
-	
-	
-	
-	
-
-
-
-
-
-
-
-	/*for(int i=0; i < height; i++)
-	{
-		for (int j = 0;j< widht; j++)
-		{
-			if (j == 0 || j == widht -1)
-			{
-				std::cout << " " << std::endl;
-			}
-			else 
-				std::cout << " " << std::endl;
-		}
-		
-	}*/
-	
-
 }
+
+void gotoxy(short int x, short int y)
+{
+	HANDLE hStdOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD position = { x, y };
+	::SetConsoleCursorPosition(hStdOut, position);
+}
+
 
 
 
 int main() 
 {
+	
+	srand(time(NULL));
 	Draw();
+	gotoxy(0, 22);
+	std::cout << "Hello" <<std::endl;
+	system("pause");
+	
 }
+
